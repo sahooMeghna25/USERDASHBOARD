1b6633eb96007b581a933fbc903e2ccc6be5bb38
Meghna sahoo <meghna.sahoo27@gmail.com>
2025-01-02 11:28:12 +0530

    update package.json and app.js

--- app.js
+++ app.js
@@ -34,9 +34,11 @@ const sessionStore = new MySQLStore({
 });
 
 // Middleware
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public", "views"));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
